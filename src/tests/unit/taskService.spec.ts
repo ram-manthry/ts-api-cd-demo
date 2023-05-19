4c56025c48945958e36024411e245eefc2c9a708
Ram Manthry <ram.manthry@gmail.com>
2023-05-19 20:20:52 +1200

    allow only PRs to main and broke a test for demo

--- src/tests/unit/taskService.spec.ts
+++ src/tests/unit/taskService.spec.ts
@@ -10,7 +10,7 @@ describe('getAllTasks', () => {
     const expected = [
       {
         id: 1,
-        name: 'Mission 01',
+        name: 'Mission 0',
         description: 'Chatbot',
         isCompleted: false,
       },
