19370033fb5072b5eb0fcb6a0a5d9754a286ee2e
oliverlelievre <oliverl@missionreadyhq.com>
2023-05-19 19:29:53 +1200

    updates

---
+++ src/tests/unit/taskController.spec.ts
@@ -0,0 +1,30 @@
+import { getAllTasks } from '../../controllers/taskController';
+import { getMockReq, getMockRes } from '@jest-mock/express';
+
+describe('getAllTasks', () => {
+    test('should send all tasks via response', () => {
+        // Arrange
+        const reqMock = getMockReq()
+        const { res } = getMockRes()
+        // Act
+        getAllTasks(reqMock, res)
+        //Assert
+        expect(res.send).toBeCalledTimes(1)
+    })
+})
+
+// describe('getOneTask', () => {
+//     test('should send 404 if it does not exist', () => {
+
+//         // Arrange
+//         const reqMock = getMockReq({ params: { id: "22" } as any })
+//         const { res } = getMockRes()
+        
+//         // Act
+//         getAllTasks(reqMock, res)
+        
+//         //Assert
+        
+//         expect(res.statusCode).toBe(404)
+//     })
+// })
